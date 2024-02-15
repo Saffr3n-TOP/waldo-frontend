947e8237128ced17b4871fc216e2121251b0787b
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-02-15 21:22:37 +0300

    Export custom fetch to use outside of hook

--- src/utils/useFetch.ts
+++ src/utils/useFetch.ts
@@ -1,39 +1,45 @@
 import { useState, useEffect } from 'react';
 
-export default function useFetch(url: string) {
+export default function useFetch() {
   const [error, setError] = useState<ApiError | undefined>();
   const [data, setData] = useState<ApiData | undefined>();
 
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
-
-    (async () => {
-      const response = await fetch(url, { signal }).catch(
-        () => new Error('Server Error')
-      );
-
-      if (response instanceof Error) {
-        return setError({ status: 500, message: response.message });
-      }
-
-      const json: ApiJson | Error = await response
-        .json()
-        .catch(() => new Error('Server Error'));
-
-      if (json instanceof Error) {
-        return setError({ status: 500, message: json.message });
-      }
-
-      if (json.error) {
-        return setError(json.error);
-      }
-
-      setData(json.data);
-    })();
-
+    waldoFetch('/', setError, setData, signal);
     return () => controller.abort();
-  }, [url]);
+  }, []);
 
-  return { error, data };
+  return { error, data, setError, setData };
+}
+
+export async function waldoFetch(
+  url: string,
+  setError: React.Dispatch<React.SetStateAction<ApiError | undefined>>,
+  setData: React.Dispatch<React.SetStateAction<ApiData | undefined>>,
+  signal?: AbortSignal
+) {
+  const response = await fetch(`http://localhost:3000${url}`, {
+    signal,
+    credentials: 'include'
+  }).catch(() => new Error('Server Error'));
+
+  if (response instanceof Error) {
+    return setError({ status: 500, message: response.message });
+  }
+
+  const json: ApiJson | Error = await response
+    .json()
+    .catch(() => new Error('Server Error'));
+
+  if (json instanceof Error) {
+    return setError({ status: 500, message: json.message });
+  }
+
+  if (json.error) {
+    return setError(json.error);
+  }
+
+  setData(json.data);
 }
